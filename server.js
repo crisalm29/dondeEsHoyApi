5946e395c6d0e8483c8bda31aa1fdcd211e2b1f1
crisalm29 <cris29sk8@gmail.com>
2017-07-06 22:17:50 -0600

    Enabled CORS

--- server.js
+++ server.js
@@ -51,11 +51,16 @@ function handle_database(req,res) {
 
 
 
-
 // Routing 
 
 
 var router = express.Router();
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 router.get('/', function(req, res){
